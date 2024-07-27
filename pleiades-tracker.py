a6bbd2dcce9c3d60d8e8152083de81d7ad907c38
crikey <111103287+crikeydev@users.noreply.github.com>
2024-07-28 00:23:59 +1000

    Initial creation of server retrieval command

--- pleiades-tracker.py
+++ pleiades-tracker.py
@@ -2,6 +2,7 @@ import logging
 import discord
 import os
 from dotenv import load_dotenv
+import a2s
 
 logging.basicConfig(level=logging.INFO)
 
@@ -21,5 +22,15 @@ async def on_ready():
 @bot.slash_command(guild_ids=[1068433342812389436])
 async def hello(ctx):
     await ctx.respond("Gday mate, you suck!")
+
+@bot.slash_command(guild_ids=[1068433342812389436])
+async def retrieve(ctx, server):
+    serverSplit = server.split(":")
+    serverIP = serverSplit[0]
+    serverPort = serverSplit[1]
+    address = (serverIP, serverPort)
+    serverInfo = a2s.info(address)
+    await ctx.respond(str(serverInfo))
+
 
 bot.run(bot_token)
